01cd5db92412e3b627e3c2bd27e1f1a9436e772c
Lusiano <115529267+lusi1011@users.noreply.github.com>
2025-10-26 06:17:10 +0700

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -14,6 +14,7 @@ st.title("Hello World")
 st.write("Welcome to Prediksi Penjualan Komputer dengan SVM")
 
 df = pd.read_csv('SuperStore_Sales_Dataset.csv')
-df_filter = (df["Category"] == "Technology") & (df["Sub-Category"] != "Phones")
 df_rename = df_filter.rename(columns={'Row ID+O6G3A1:R6' : 'Row ID'})
+df_filter = (df["Category"] == "Technology") & (df["Sub-Category"] != "Phones")
+
 st.dataframe(df_rename)
