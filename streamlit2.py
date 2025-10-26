7acb66053fe93314169e6f98f87db1e4179da76b
Lusiano <115529267+lusi1011@users.noreply.github.com>
2025-10-26 11:09:18 +0700

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -14,5 +14,5 @@ st.title("Hello World")
 st.write("Welcome to Prediksi Penjualan Komputer dengan SVM")
 
 df = pd.read_csv('SuperStore_Sales_Dataset.csv')
-df.rename(columns={'Row ID+O6G3A1:R6': 'Row ID')
+df.rename(columns={'Row ID+O6G3A1:R6': 'Row ID'})
 st.write(df)
