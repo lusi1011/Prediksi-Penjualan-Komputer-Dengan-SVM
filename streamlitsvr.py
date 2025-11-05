4947a2abe6df60edf84481ef9106db911d257431
Lusiano <115529267+lusi1011@users.noreply.github.com>
2025-11-05 20:35:11 +0700

    Update streamlitsvr.py

--- streamlitsvr.py
+++ streamlitsvr.py
@@ -150,8 +150,8 @@ def run_all_svr_analysis(df_clean, selected_feature, selected_target):
     for kernel in kernels:
         # Konfigurasi parameter default
         if kernel == 'linear': svr = SVR(kernel=kernel, C=10)
-        elif kernel == 'poly': svr = SVR(kernel=kernel, C=50, degree=2, gamma='auto')
-        elif kernel == 'rbf': svr = SVR(kernel=kernel, C=50, gamma='auto')
+        elif kernel == 'poly': svr = SVR(kernel=kernel, C=10, degree=2, gamma='auto')
+        elif kernel == 'rbf': svr = SVR(kernel=kernel, C=10, gamma='auto')
         elif kernel == 'sigmoid': svr = SVR(kernel=kernel, C=1, gamma=0.1, coef0=0)
             
         # --- PERBAIKAN KRITIS: BLOK YANG HILANG DIKEMBALIKAN ---
